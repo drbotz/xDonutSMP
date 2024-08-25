4c630deba5f40a7686ef416144a240221fc1f35c
Obecalp <99992836+obecalpjava@users.noreply.github.com>
2024-08-25 19:23:39 -0400

    Delete dist directory

--- dist/buttons/cancel.js
+++
@@ -1,8 +0,0 @@
-export default {
-    id: "cancel",
-    function: async function ({ button }) {
-        await button.reply({ content: ":white_check_mark: **The scam report has been cancelled.**" });
-        await new Promise(resolve => setTimeout(resolve, 1000));
-        await button.channel.delete();
-    }
-};
